dbdd89af192545957094b746b478f11c546ed43e
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-08-22 21:09:01 +0900

    fix main func.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <solver.h>
 
 int main() {
-    auto s = std::make_shared<Solver>(6, 5);
+    auto s = std::make_shared<Solver>(6, 10);
     std::vector<char> body = {'a', ' ',
                               'a', ' ', 
                               'a', 'a',
@@ -39,39 +39,39 @@ int main() {
     piece = std::make_shared<Piece>(5, 1, body);
     s->addPiece(piece);
 
-    // body = {'g', 'g', 'g', ' ',
-    //         ' ', ' ', 'g', 'g'};
-    // piece = std::make_shared<Piece>(4, 2, body);
-    // s->addPiece(piece);
+    body = {'g', 'g', 'g', ' ',
+            ' ', ' ', 'g', 'g'};
+    piece = std::make_shared<Piece>(4, 2, body);
+    s->addPiece(piece);
 
-    // body = {'h', 'h', 'h',
-    //         ' ', 'h', 'h'};
-    // piece = std::make_shared<Piece>(3, 2, body);
-    // s->addPiece(piece);
+    body = {'h', 'h', 'h',
+            ' ', 'h', 'h'};
+    piece = std::make_shared<Piece>(3, 2, body);
+    s->addPiece(piece);
 
-    // body = {'i', 'i', ' ',
-    //         ' ', 'i', 'i',
-    //         ' ', 'i', ' '};
-    // piece = std::make_shared<Piece>(3, 3, body);
-    // s->addPiece(piece);
+    body = {'i', 'i', ' ',
+            ' ', 'i', 'i',
+            ' ', 'i', ' '};
+    piece = std::make_shared<Piece>(3, 3, body);
+    s->addPiece(piece);
 
-    // body = {'j', ' ', ' ',
-    //         'j', ' ', ' ',
-    //         'j', 'j', 'j'};
-    // piece = std::make_shared<Piece>(3, 3, body);
-    // s->addPiece(piece);
+    body = {'j', ' ', ' ',
+            'j', ' ', ' ',
+            'j', 'j', 'j'};
+    piece = std::make_shared<Piece>(3, 3, body);
+    s->addPiece(piece);
 
-    // body = {' ', 'k', ' ',
-    //         'k', 'k', 'k',
-    //         ' ', 'k', ' '};
-    // piece = std::make_shared<Piece>(3, 3, body);
-    // s->addPiece(piece);
+    body = {' ', 'k', ' ',
+            'k', 'k', 'k',
+            ' ', 'k', ' '};
+    piece = std::make_shared<Piece>(3, 3, body);
+    s->addPiece(piece);
 
-    // body = {'l', 'l',
-    //         ' ', 'l',
-    //         'l', 'l'};
-    // piece = std::make_shared<Piece>(2, 3, body);
-    // s->addPiece(piece);
+    body = {'l', 'l',
+            ' ', 'l',
+            'l', 'l'};
+    piece = std::make_shared<Piece>(2, 3, body);
+    s->addPiece(piece);
 
     s->solve();
     return 0;
