97fa6dd819a3bfa66ddb38bb7c966fbbc0f8bd48
Shinya Hayashi <shinyaUT0925@gmail.com>
2022-08-23 23:13:26 +0900

    add test for pieceCollection.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- test_src/piece_collection_test.cpp
+++ test_src/piece_collection_test.cpp
@@ -13,4 +13,25 @@ TEST(PieceCollectionTest, constructor)
 
     EXPECT_EQ(4, pc->size());
     EXPECT_FALSE(pc->isEnd());
+
+    for(int i = 0; i < 4; i++) {
+        pc->next();
+    }
+    EXPECT_TRUE(pc->isEnd());
+
+    body = {' ', 'd', ' ',
+            'd', 'd', 'd',
+            ' ', 'd', ' '};
+    piece = std::make_shared<Piece>(3, 3, body);
+
+    pc = std::make_shared<PieceCollection>(piece);
+
+    EXPECT_EQ(1, pc->size());
+    EXPECT_FALSE(pc->isEnd());
+    pc->next();
+    EXPECT_TRUE(pc->isEnd());
+    pc->reset();
+    EXPECT_FALSE(pc->isEnd());
+    pc->next();
+    EXPECT_TRUE(pc->isEnd());
 }
